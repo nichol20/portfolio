abd13ce34e024375a63698a247b4a785ff19a75f
Nicholas <nick.nogue28@outlook.com>
2024-01-28 23:07:58 -0300

    move back translations and remove i18next-http-backend

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,15 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import HttpApi from "i18next-http-backend";
 import { supportedLngs } from "./data/languages";
+
+import pt from "./assets/locales/pt/translation.json";
+import en from "./assets/locales/en/translation.json";
+import ja from "./assets/locales/ja/translation.json";
 
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
-  .use(HttpApi)
   .init({
     supportedLngs: supportedLngs.map((lng) => lng.code),
     fallbackLng: "pt",
@@ -15,7 +17,15 @@ i18n
       order: ["path", "cookie", "htmlTag"],
       caches: ["cookie"],
     },
-    backend: {
-      loadPath: "/locales/{{lng}}/translation.json",
+    resources: {
+      pt: {
+        translation: pt,
+      },
+      en: {
+        translation: en,
+      },
+      ja: {
+        translation: ja,
+      },
     },
   });
