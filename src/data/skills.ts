be02a46982e31e860f96ce4167d9ef768114aa9f
Nicholas <nick.nogue28@outlook.com>
2024-01-28 23:52:31 -0300

    format

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,168 +1,197 @@
-import { abcIcon, csharpIcon, cssIcon, cypressIcon, dockerIcon, expressjsIcon, gitIcon, golangIcon, htmlIcon, javaIcon, javascriptIcon, jestIcon, jwtIcon, kubernetesIcon, linuxIcon, mongodbIcon, nextjsIcon, nginxIcon, nodejsIcon, postgresqlIcon, pythonIcon, rabbitmqIcon, reactIcon, redisIcon, reduxIcon, sassIcon, socketioIcon, testingLibraryIcon, typescriptIcon } from "../assets/images";
+import {
+  abcIcon,
+  csharpIcon,
+  cssIcon,
+  cypressIcon,
+  dockerIcon,
+  expressjsIcon,
+  gitIcon,
+  golangIcon,
+  htmlIcon,
+  javaIcon,
+  javascriptIcon,
+  jestIcon,
+  jwtIcon,
+  kubernetesIcon,
+  linuxIcon,
+  mongodbIcon,
+  nextjsIcon,
+  nginxIcon,
+  nodejsIcon,
+  postgresqlIcon,
+  pythonIcon,
+  rabbitmqIcon,
+  reactIcon,
+  redisIcon,
+  reduxIcon,
+  sassIcon,
+  socketioIcon,
+  testingLibraryIcon,
+  typescriptIcon,
+} from "../assets/images";
 import { SkillCategoryObject } from "../types/data";
 
 export const frontend: SkillCategoryObject = {
   html: {
     icon: htmlIcon,
-    name: 'HTML',
-    percentage: 90
+    name: "HTML",
+    percentage: 90,
   },
   css: {
     icon: cssIcon,
-    name: 'CSS',
-    percentage: 90
+    name: "CSS",
+    percentage: 90,
   },
   javascript: {
     icon: javascriptIcon,
-    name: 'Javascript',
-    percentage: 90
+    name: "Javascript",
+    percentage: 90,
   },
   react: {
     icon: reactIcon,
-    name: 'React',
-    percentage: 80
+    name: "React",
+    percentage: 80,
   },
   nextjs: {
     icon: nextjsIcon,
-    name: 'NextJS',
-    percentage: 75
+    name: "NextJS",
+    percentage: 75,
   },
   sass: {
     icon: sassIcon,
-    name: 'SASS',
-    percentage: 60
+    name: "SASS",
+    percentage: 60,
   },
   testingLibrary: {
     icon: testingLibraryIcon,
-    name: 'Testing library',
-    percentage: 50
+    name: "Testing library",
+    percentage: 50,
   },
   cypress: {
     icon: cypressIcon,
-    name: 'Cypress',
-    percentage: 40
+    name: "Cypress",
+    percentage: 40,
   },
   redux: {
     icon: reduxIcon,
-    name: 'Redux',
-    percentage: 35
-  }
-}
+    name: "Redux",
+    percentage: 35,
+  },
+};
 
 export const backend: SkillCategoryObject = {
   nodejs: {
     icon: nodejsIcon,
-    name: 'NodeJS',
-    percentage: 80
+    name: "NodeJS",
+    percentage: 80,
   },
   mongo: {
     icon: mongodbIcon,
-    name: 'MongoDB',
-    percentage: 80
+    name: "MongoDB",
+    percentage: 80,
   },
   expressjs: {
     icon: expressjsIcon,
-    name: 'ExpressJS',
-    percentage: 65
+    name: "ExpressJS",
+    percentage: 65,
   },
   docker: {
     icon: dockerIcon,
-    name: 'Docker',
-    percentage: 65
+    name: "Docker",
+    percentage: 65,
   },
   rabbitmq: {
     icon: rabbitmqIcon,
-    name: 'RabbitMQ',
-    percentage: 40
+    name: "RabbitMQ",
+    percentage: 40,
   },
   redis: {
     icon: redisIcon,
-    name: 'Redis',
-    percentage: 40
+    name: "Redis",
+    percentage: 40,
   },
   postgres: {
     icon: postgresqlIcon,
-    name: 'PostgreSQL',
-    percentage: 40
+    name: "PostgreSQL",
+    percentage: 40,
   },
   kubernetes: {
     icon: kubernetesIcon,
-    name: 'Kubernetes',
-    percentage: 20
+    name: "Kubernetes",
+    percentage: 20,
   },
   nginx: {
     icon: nginxIcon,
-    name: 'Nginx',
-    percentage: 20
-  }
-}
+    name: "Nginx",
+    percentage: 20,
+  },
+};
 
 export const general: SkillCategoryObject = {
   english: {
     icon: abcIcon,
-    name: 'English',
-    percentage: 80
+    name: "English",
+    percentage: 80,
   },
   typescript: {
     icon: typescriptIcon,
-    name: 'Typescript',
-    percentage: 80
+    name: "Typescript",
+    percentage: 80,
   },
   git: {
     icon: gitIcon,
-    name: 'GIT',
-    percentage: 75
+    name: "GIT",
+    percentage: 75,
   },
   go: {
     icon: golangIcon,
     name: "Golang",
-    percentage: 50
+    percentage: 50,
   },
   python: {
     icon: pythonIcon,
-    name: 'Python',
-    percentage: 50
+    name: "Python",
+    percentage: 50,
   },
   socketio: {
     icon: socketioIcon,
-    name: 'Socket.io',
-    percentage: 45
+    name: "Socket.io",
+    percentage: 45,
   },
   jwt: {
     icon: jwtIcon,
-    name: 'Json web token',
-    percentage: 45
+    name: "Json web token",
+    percentage: 45,
   },
   linuxCommands: {
     icon: linuxIcon,
-    name: 'Linux commands',
-    percentage: 45
+    name: "Linux commands",
+    percentage: 45,
   },
   jest: {
     icon: jestIcon,
-    name: 'Jest',
-    percentage: 40
+    name: "Jest",
+    percentage: 40,
   },
   japanese: {
     icon: abcIcon,
-    name: '日本語',
-    percentage: 30
+    name: "日本語",
+    percentage: 30,
   },
   csharp: {
     icon: csharpIcon,
-    name: 'C#',
-    percentage: 20
+    name: "C#",
+    percentage: 20,
   },
   java: {
     icon: javaIcon,
-    name: 'Java',
-    percentage: 20
+    name: "Java",
+    percentage: 20,
   },
-
-}
+};
 
 export default {
   frontend,
   backend,
-  general
-}
+  general,
+};
